ea57426cbbfdb6cb0c9a84d4bad749f233458e1f
root <wheakerd@gmail.com>
2025-10-19 17:51:30 +0800

    fix: Fixing the infinite loop.

--- src/ApplicationFactory.php
+++ src/ApplicationFactory.php
@@ -3,22 +3,17 @@ declare (strict_types=1);
 
 namespace SuperKernel\Framework;
 
-use Psr\EventDispatcher\EventDispatcherInterface;
 use SuperKernel\Attribute\Factory;
 use SuperKernel\Attribute\Provider;
 use SuperKernel\Contract\ApplicationInterface;
-use SuperKernel\Event\BootApplication;
-
 #[
     Provider(ApplicationInterface::class),
     Factory,
 ]
 final readonly class ApplicationFactory
 {
-    public function __invoke(Application $application, EventDispatcherInterface $eventDispatcher): ApplicationInterface
+    public function __invoke(Application $application): ApplicationInterface
     {
-        $eventDispatcher->dispatch(new BootApplication());
-
         return $application;
     }
 }
