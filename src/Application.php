7fcf0a5c98adbbc3e1e3824330f36cd1d679aed1
root <wheakerd@gmail.com>
2025-10-19 17:46:56 +0800

    fix: Fixing the infinite loop.

--- src/Application.php
+++ src/Application.php
@@ -3,8 +3,23 @@ declare(strict_types=1);
 
 namespace SuperKernel\Framework;
 
+use Psr\EventDispatcher\EventDispatcherInterface;
 use SuperKernel\Contract\ApplicationInterface;
+use SuperKernel\Event\BootApplication;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 final class Application extends \Symfony\Component\Console\Application implements ApplicationInterface
 {
+	public function __construct(private readonly EventDispatcherInterface $eventDispatcher)
+	{
+		parent::__construct();
+	}
+
+	public function run(InputInterface|null $input = null, OutputInterface|null $output = null): int
+	{
+		$this->eventDispatcher->dispatch(new BootApplication());
+
+		return parent::run($input, $output);
+	}
 }
