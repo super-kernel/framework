8d3658c67ddf3a20027bdd638b9f92b6655aabd2
root <wheakerd@gmail.com>
2025-10-19 16:04:20 +0800

    feat: Optimize and upgrade version v1.1

--- src/Application.php
+++ src/Application.php
@@ -4,26 +4,21 @@ declare(strict_types=1);
 namespace SuperKernel\Framework;
 
 use Psr\EventDispatcher\EventDispatcherInterface;
+use SuperKernel\Attribute\Factory;
+use SuperKernel\Attribute\Provider;
 use SuperKernel\Contract\ApplicationInterface;
 use SuperKernel\Event\BootApplication;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
+#[
+	Provider(ApplicationInterface::class),
+	Factory,
+]
 final class Application extends \Symfony\Component\Console\Application implements ApplicationInterface
 {
-    private readonly EventDispatcherInterface $eventDispatcher;
+	public function __invoke(EventDispatcherInterface $eventDispatcher): ApplicationInterface
+	{
+		$eventDispatcher->dispatch(new BootApplication());
 
-    public function __construct(EventDispatcherInterface $eventDispatcher, string $name = 'UNKNOWN', string $version = 'UNKNOWN')
-    {
-        $this->eventDispatcher = $eventDispatcher;
-
-        parent::__construct($name, $version);
-    }
-
-    public function run(?InputInterface $input = null, ?OutputInterface $output = null): int
-    {
-        $this->eventDispatcher->dispatch(new BootApplication());
-
-        return parent::run($input, $output);
-    }
+		return $this;
+	}
 }
